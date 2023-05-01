9005ac1ae9a8929a60a2f05c051b6429439034b5
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-01 10:14:26 +0100

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 const welcomeGreetings = "Welcome to Note Management";
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <div className='App'>
       <div className='content'>
         <h1>{welcomeGreetings}</h1>
+        <Navbar/>
+        <a href='/'>Return Home</a>
       </div>
     </div>
   );
