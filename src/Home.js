2173c7d15b24efc2dca9d018e2f29823c61ba980
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-06 23:24:17 +0100

    setting loadin message

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,11 @@ import BlogList from "./BlogList";
 
 function Home(){
   const [blogs, setBlogs] = useState(null);
-  const handleBlogDelete=(blogId)=>{
-    const newBlogs = blogs.filter(blog=>blog.id !== blogId)
-    setBlogs(newBlogs)
-  }
+  const [isPending, setPending] = useState(true);
+  // const handleBlogDelete=(blogId)=>{
+  //   const newBlogs = blogs.filter(blog=>blog.id !== blogId)
+  //   setBlogs(newBlogs)
+  // }
   useEffect(()=>{
 fetch("http://localhost:8000/blogs").then(res =>{
  return res.json();
