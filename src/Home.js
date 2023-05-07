c414bcd5f75d89f5a48bd1c94fb63b7d0b642948
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-07 07:24:42 +0100

    catching fecthed data error

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,14 @@ import BlogList from "./BlogList";
 function Home(){
   const [blogs, setBlogs] = useState(null);
   const [isPending, setPending] = useState(true);
+  const [error, setError] = useState(null);
   // const handleBlogDelete=(blogId)=>{
   //   const newBlogs = blogs.filter(blog=>blog.id !== blogId)
   //   setBlogs(newBlogs)
   // }
   useEffect(()=>{
     setTimeout(()=>{
-      fetch("http://localhost:8000/blogs").then(res =>{
+      fetch("http://localhost:8000/blogss").then(res =>{
         if(!res.ok){
           throw Error("Can't fecth data");
         }
@@ -19,13 +20,14 @@ function Home(){
   setBlogs(data);
   setPending(false);
 }).catch(err =>{
-  console.log(err.message);
+  setError(err.message);
 })
 
     }, 1000)
   }, [])
   return(
     <div className="home">
+      {error && <div>{error}</div> }
       {isPending && <div>loading......</div>}
      {blogs && <BlogList blogs={blogs} title="List of Blogs"/>}
      {/* <BlogList blogs={blogs.filter((blog)=>blog.author === "Mubarak")} title="Mubarak's blogs" handleBlogDelete={handleBlogDelete}/> */}
