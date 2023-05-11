05120d97e14b1af9d2f1e0c472e11e837e673d01
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-11 16:03:11 +0100

    creating route parameter

--- src/CreateBlogNinja.js
+++ src/CreateBlogNinja.js
@@ -1,7 +1,25 @@
 const CreateBlog = () => {
   return (  
-    <div className="createBlog">
+    <div className="create-blog">
       <h2>Add a new blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+        type="text"
+        required
+        />
+         <label>Blog body:</label>
+       <textarea>
+        required
+       </textarea>
+         <label>Blog author:</label>
+        <select>
+          <option value="Mubarak">Mubarak</option>
+          <option value="Oladejo">Oladejo</option>
+        </select>
+        <button>Add blog</button>
+      </form>
+
     </div>
   );
 }
