118011b4c2b3c698aa23d3e03a9aea556b0c05df
oladejomubarak <oladejomubarakade@gmail.com>
2023-05-12 12:35:02 +0100

    adding form

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+const Notfound = () => {
+  return (  
+    <div className="not-found">
+      <h1>Soryy...</h1>
+      <p>This page can not be found</p>
+      <Link to = '/'>Bak to home page...</Link>
+    </div>
+  );
+}
+ 
+export default Notfound;
